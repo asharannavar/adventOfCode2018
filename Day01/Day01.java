45038b8694e5186728cb0a9f73ce178e608341fa
Anand Sharannavar <sharan.anand@gmail.com>
2018-12-09 22:50:02 -0800

    Updated file

--- Day01/Day01.java
+++ Day01/Day01.java
@@ -2,35 +2,46 @@ package adventOfCode;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Day01 {
+	int frequency = 0;
+	List<Integer> list = new ArrayList<Integer>();
 
 	public static void main(String[] args) throws IOException {
 		
-		try{
-			File f = new File("Day01_Input.txt");
+	Day01 day1 = new Day01();	
+	while (!day1.readFile()) {
+		System.out.println("Frequency = " +day1.frequency);
+		
+	}
+
+	}
+	
+	public boolean readFile() throws IOException {
+		File f = new File("Day01_Input.txt");
 		
 		BufferedReader br = new BufferedReader(new FileReader(f));
-		
-		int sum = 0;
 		String line = br.readLine();
-	
-		while(line != null) {
-			sum += Integer.parseInt(line);
+		
+		while(line != null) {			
+			Integer sum = Integer.valueOf(line);
+			frequency += sum; 
+			if(!list.contains(frequency)) {
+				list.add(frequency);
+			} else {
+				System.out.println("Duplicate = " +frequency);
+				return true;
+			}
 			line = br.readLine();
 		}
 			br.close();
-			System.out.println("Sum = " +sum);
+			return false;
 		
 		}
-		
-		catch (IOException e) {
-            e.printStackTrace();
-        }                       
-		
-
-	}
 
 }
